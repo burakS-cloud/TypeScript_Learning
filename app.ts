4a3030b9e4a8fd75323d08d9574b1639e1a69227
burakS-cloud <bsurumcuoglu@gmail.com>
2022-09-30 12:28:19 +0300

    enum types

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Burak",
-  age: 25,
-  hobbies: ["Fitness", "Gaming"],
-  role: [2, "author"],
-};
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Burak",
+//   age: 25,
+//   hobbies: ["Fitness", "Gaming"],
+//   role: [2, "author"],
+// };
 
 // This role's type is a tuple. Means the first one must be a number
 //second one must be a string
@@ -19,3 +25,17 @@ const person: {
 
 //Running push method on person.role breaks this rule
 //typescript can't catch that and the length will be increased.
+
+const person = {
+  name: "Burak",
+  age: 25,
+  hobbies: ["Fitness", "Gaming"],
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
+}
+
+//ENUM Type saves us from having to manage the global constants and remembering the variable names
+// to use it while doing some conditionals.
