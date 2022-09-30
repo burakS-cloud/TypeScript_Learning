7e55f06d3d609de438c41f36ab15dd68ce49ec8a
burakS-cloud <bsurumcuoglu@gmail.com>
2022-09-30 12:16:21 +0300

    learn about typescript type interferences

--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-console.log("your code goes here");
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Burak",
+  age: 25,
+  hobbies: ["Fitness", "Gaming"],
+  role: [2, "author"],
+};
+
+// This role's type is a tuple. Means the first one must be a number
+//second one must be a string
+
+//This would be throwing an error because the length of role must be exactly 0
+
+// person.role =  [0, 'admin', 'user']
+
+//Running push method on person.role breaks this rule
+//typescript can't catch that and the length will be increased.
